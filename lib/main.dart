fdccd9479de48e908c326414eeac8b62e328217c
Hhurtadoabcx <contacthurtadolxyz@gmail.com>
2023-10-03 18:59:36 -0400

    AppBar, TabBar y cards agregados

---
+++ lib/main.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: Color.fromARGB(255, 223, 5, 96),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Bookings'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Widget> _buildCardList(int count) {
+    List<Widget> cards = [];
+
+    for (int i = 1; i <= count; i++) {
+      cards.add(
+        Card(
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundColor: Color.fromARGB(115, 209, 88, 138),
+              child: Icon(Icons.airplanemode_active, color: Color.fromARGB(255, 223, 5, 96)),
+            ),
+            title: Text('Título $i'),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('28 Oct. 2018 ----- 11:00AM'),
+                Text('Emirates Airways'),
+              ],
+            ),
+            trailing: Icon(Icons.more_vert),
+          ),
+        ),
+      );
+    }
+
+    return cards;
+  }
+
+  @override
+  Widget build(BuildContext context) => DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(widget.title),
+            leading: IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () {
+              },
+            ),
+            actions: [
+              IconButton(
+                icon: Icon(Icons.notifications),
+                onPressed: () {
+                },
+              ),
+              IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () {
+                },
+              ),
+            ],
+            centerTitle: true,
+            bottom: TabBar(
+              tabs: [
+                Tab(text: 'TRIPS'),
+                Tab(text: 'HOTELS'),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              ListView(
+                children: _buildCardList(10),
+              ),
+              ListView(
+                children: _buildCardList(10),
+              ),
+            ],
+          ),
+        ),
+      );
+}
